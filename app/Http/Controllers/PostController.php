cbcce9bcc2be11700a81d673d34019dc0d2f20a8
George Kalandadze <kalandadzeg777@gmail.com>
2024-06-19 19:27:05 +0400

    feat/make post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,69 +2,63 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): JsonResponse
+    public function index()
     {
-        $posts = Post::all();
-        return response()->json($posts);
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): JsonResponse
+    public function store(Request $request)
     {
-        $request->validate([
-            'title' => 'required|string|max:255',
-            'body' => 'required|string',
-        ]);
-
-        $post = Post::create($request->all());
-
-        return response()->json($post, 201);
+        //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id): JsonResponse
+    public function show(string $id)
     {
-        $post = Post::findOrFail($id);
-        return response()->json($post);
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id): JsonResponse
+    public function update(Request $request, string $id)
     {
-        $request->validate([
-            'title' => 'required|string|max:255',
-            'body' => 'required|string',
-        ]);
-
-        $post = Post::findOrFail($id);
-        $post->update($request->all());
-
-        return response()->json($post);
+        //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id): JsonResponse
+    public function destroy(string $id)
     {
-        $post = Post::findOrFail($id);
-        $post->delete();
-
-        return response()->json(null, 204);
+        //
     }
 }
